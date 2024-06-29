c72e96bc1aef475edfaa7a68d1d77fcd08fe6c46
Esduardo <eduartrob@gmail.com>
2024-06-29 14:53:07 -0600

    update-name-routes

--- src/ModuleBovino/infrastructure/routes/bovinoRouter.ts
+++ src/ModuleBovino/infrastructure/routes/bovinoRouter.ts
@@ -3,9 +3,9 @@ import { bovinoController } from "../dependencies";
 
 export const bovinoRouter = express.Router();
 
-bovinoRouter.get("/all", bovinoController.getAllBovinos.bind(bovinoController));
-bovinoRouter.post("/name", bovinoController.getBovino.bind(bovinoController));
+bovinoRouter.get("/get-all", bovinoController.getAllBovinos.bind(bovinoController));
+bovinoRouter.post("/get-for-name", bovinoController.getBovino.bind(bovinoController));
 bovinoRouter.post("/create", bovinoController.createBovino.bind(bovinoController));
 bovinoRouter.put("/update-data/:name", bovinoController.updateBovino.bind(bovinoController));
 bovinoRouter.delete("/delete-bovino/:name", bovinoController.deleteBovino.bind(bovinoController));
-bovinoRouter.delete("/all", bovinoController.deleteAllBovinos.bind(bovinoController));
+bovinoRouter.delete("/delete-all", bovinoController.deleteAllBovinos.bind(bovinoController));
